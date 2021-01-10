c4be9067babdf98b568fd5977f7e899b78bce42e
ZamiRich <argashokovzamir@gmail.com>
2021-01-10 19:19:42 +0100

    Initial Commit

---
+++ SixthLesson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SixthLesson
+//
+//  Created by ZamiRich on 10.01.2021.
+//  Copyright © 2021 ZamiRich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
